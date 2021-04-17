1f15d7523ab4354f2c1f55fcdd4327417d53c525
ilyak93 <ilyak93@gmail.com>
2021-04-17 22:09:55 +0000

    cosmetic path fixes

--- BaseCAM.py
+++ BaseCAM.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 import torch
-from pytorch_grad_cam.activations_and_gradients import ActivationsAndGradients
+from activations_and_gradients import ActivationsAndGradients
+
 
 class BaseCAM:
     def __init__(self, model, target_layer, use_cuda=False):
@@ -41,7 +42,7 @@ class BaseCAM:
 
         activations = self.activations_and_grads.activations[-1].cpu().data.numpy()[0, :]
         grads = self.activations_and_grads.gradients[-1].cpu().data.numpy()[0, :]
-        
+
         weights = self.get_cam_weights(input_tensor, target_category, activations, grads)
         cam = np.zeros(activations.shape[1:], dtype=np.float32)
 

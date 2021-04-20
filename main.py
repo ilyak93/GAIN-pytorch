44087371bd4f757ac74524b75f95898540511dba
Student2 <voldman@campus.technion.ac.il>
2021-04-20 18:12:21 +0000

    update main

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ from utils.image import show_cam_on_image, preprocess_image, deprocess_image
 
 from models.gcam import GCAM
 from PIL import Image
-
-
-
+from tensorboardX import SummaryWriter
 
 
 
@@ -27,9 +25,7 @@ def main():
             ]
 
     num_classes = len(categories)
-
     device = torch.device('cuda:0')
-
     model = vgg19(pretrained=True).train().to(device)
 
     #model = mobilenet_v2(pretrained=True).train().to(device)
@@ -37,14 +33,10 @@ def main():
     #change the last layer for finetuning
     classifier = model.classifier
     num_ftrs = classifier[-1].in_features
-
     new_classifier = torch.nn.Sequential(*(list(model.classifier.children())[:-1]), nn.Linear(num_ftrs, num_classes).to(device))
-
     model.classifier = new_classifier
-
     model.train()
-
-    target_layer = model.features[-1]
+    # target_layer = model.features[-1]
 
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
@@ -58,68 +50,61 @@ def main():
     #input_tensor = torch.from_numpy(np_im).unsqueeze(0).permute([0,3,1,2]).to(device).float()
     #np_im = input_tensor.squeeze().permute(1,2,0).cpu()
 
-    '''
-    cam = GradCAM(model=model, target_layer=target_layer, use_cuda=True)
-    grayscale_cam = cam(input_tensor=input_tensor, target_category=0)
-    visualization, heatmap = show_cam_on_image(np_im, grayscale_cam, True)
-    plt.imshow(visualization)
-    plt.show()
-    plt.imshow(heatmap)
-    plt.show()
-
-    target_class = 0
-
-    gcam = Grad_CAM(model=model)
-    for i in range(3):
-        gcam.forward(input_tensor)
-        ids_ = torch.LongTensor([[target_class]] * len(input_tensor)).to(device)
-        gcam.backward(ids=ids_)
-        regions = gcam.generate(target_layer='features')
-        regions_cpu = regions.squeeze().cpu()
-        visualization, heatmap = show_cam_on_image(np_im, regions_cpu, True)
-        plt.imshow(visualization)
-        plt.show()
-        plt.imshow(heatmap)
-        plt.show()
-        print()
-    '''
 
     dataset_path = 'C:/VOC-dataset'
     input_dims = [224, 224]
     batch_size_dict = {'train': 1, 'test': 1}
-
     rds = data.RawDataset(root_dir=dataset_path,
                           num_workers=0,
                           output_dims=input_dims,
                           batch_size_dict=batch_size_dict)
 
+    num_train_samples = len(rds.datasets['train'])
+    print(num_train_samples)
+
+    num_test_samples = len(rds.datasets['test'])
+    print(num_test_samples)
+
+
     epochs = 15
-
-
     loss_fn = torch.nn.BCEWithLogitsLoss()
-
     optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
-
-    #gcam = Grad_CAM(model=model)
-
-    gcam = GCAM(model=model, grad_layer='features', num_classes=20)
-
+    # gcam = GCAM(model=model, grad_layer='features', num_classes=20)
     total_train_loss = []
     total_train_accuracy = []
     total_test_loss = []
     total_test_accuracy = []
 
-    viz_path = 'C:/Users/Student1/PycharmProjects/GCAM/viz'
+    epoch_train_single_accuracy = []
+    epoch_train_multi_accuracy = []
+    epoch_test_single_accuracy = []
+    epoch_test_multi_accuracy = []
+
+
+    viz_path = 'C:/Users/Student2/PycharmProjects/GCAM/exp1'
     pathlib.Path(viz_path).mkdir(parents=True, exist_ok=True)
 
     for epoch in range(epochs):
+        total_train_single_accuracy = 0
+        total_train_multi_accuracy = 0
+        total_test_single_accuracy = 0
+        total_test_multi_accuracy = 0
+
+
         train_accuracy = []
         mean_train_accuracy = []
         test_accuracy = []
         mean_test_accuracy = []
         train_epoch_loss = []
         test_epoch_loss = []
-        train_path = 'C:/Users/Student1/PycharmProjects/GCAM/viz/train'
+
+        train_multi_accuracy = []
+        mean_train_multi_accuracy = []
+        test_multi_accuracy = []
+        mean_test_multi_accuracy = []
+
+
+        train_path = 'C:/Users/Student2/PycharmProjects/GCAM/exp1/train'
         pathlib.Path(train_path).mkdir(parents=True, exist_ok=True)
         epoch_path = train_path+'/epoch_'+str(epoch)
         pathlib.Path(epoch_path).mkdir(parents=True, exist_ok=True)
@@ -128,79 +113,49 @@ def main():
         i = 0
         for sample in rds.datasets['train']:
             input_tensor = preprocess_image(sample['image'].squeeze().numpy(), mean=mean, std=std).to(device)
-            #input_tensor2 = torch.from_numpy(sample['image'].copy()).unsqueeze(0).permute([0, 3, 1, 2]).to(device).float()
-            #np_im2 = input_tensor.squeeze().permute(1, 2, 0).cpu()
             label_idx_list = sample['label/idx']
             num_of_labels = len(label_idx_list)
-
-            """
-            print(i)
-            if i % 100 == 0 :
-                img = sample['image']
-                plt.imshow(img)
-                plt.show()
-
-                for label_idx in label_idx_list:
-                    print(categories[label_idx])
-    
-    
-                    gcam.forward(input_tensor)
-                    ids_ = torch.LongTensor([[label_idx]] * len(input_tensor)).to(device)
-                    gcam.backward(ids=ids_)
-                    regions = gcam.generate(target_layer='features')
-                    regions_cpu = regions.squeeze().cpu()
-                    visualization, heatmap = show_cam_on_image(img, regions_cpu, True)
-                    plt.imshow(visualization)
-                    plt.show()
-                    plt.imshow(heatmap)
-                    plt.show()
-    
-    
-                    '''
-                    cam = GradCAM(model=model, target_layer=target_layer, use_cuda=True)
-                    grayscale_cam = cam(input_tensor=input_tensor, target_category=label_idx)
-                    heatmap = cv2.applyColorMap(np.uint8(255 * grayscale_cam), cv2.COLORMAP_JET)
-                    heatmap = np.float32(heatmap) / 255
-                    plt.imshow(heatmap)
-                    plt.show()
-                    visualization = show_cam_on_image(img, heatmap)
-                    plt.imshow(visualization)
-                    plt.show()
-                    print()
-                    '''
-            """
-
             optimizer.zero_grad()
-
             labels = torch.Tensor(label_idx_list).to(device).long()
 
-            logits, heatmap = gcam(input_tensor, labels)
-
+            logits = model(input_tensor)
+            # logits, heatmap = gcam(input_tensor, labels)
             indices = torch.Tensor(label_idx_list).long().to(device)
             class_onehot = torch.nn.functional.one_hot(indices, num_classes).sum(dim=0).unsqueeze(0).float()
 
             loss = loss_fn(logits, class_onehot)
-
             loss.backward()
-
             optimizer.step()
+
+            # Single label evaluation
+            y_pred = logits.detach().argmax()
+            y_pred = y_pred.view(-1)
+            gt, _ = indices.sort(descending=True)
+            gt = gt.view(-1)
+            acc = (y_pred == gt).sum()
+            total_train_single_accuracy += acc
+
+            # Multi label evaluation
+            _, y_pred_multi = logits.detach().topk(num_of_labels)
+            y_pred_multi = y_pred_multi.view(-1)
+            acc_multi = (y_pred_multi == gt).sum() / num_of_labels
+            total_train_multi_accuracy += acc_multi
 
             if i % 100 == 0:
                 print(i)
                 print('Loss per image: {:.3f}'.format(loss.detach().item()))
 
-                _, y_pred = logits.detach().topk(num_of_labels)
-                y_pred = y_pred.view(-1)
-                gt, _ = indices.sort(descending=True)
-                gt = gt.view(-1)
-                acc = (y_pred == gt).sum() / num_of_labels
                 train_accuracy.append(acc)
+                train_multi_accuracy.append(acc_multi)
+
 
             if i % 200 == 0:
                 train_epoch_loss.append(loss.detach().item())
                 if len(train_accuracy) > 1:
                     mean_train_accuracy.append(sum(train_accuracy) / len(train_accuracy))
-                    print('Average accuracy: {:.3f}'.format(sum(train_accuracy) / len(train_accuracy)))
+                    mean_train_multi_accuracy.append(sum(train_multi_accuracy) / len(train_multi_accuracy))
+                    print('Average train single label accuracy: {:.3f}'.format(sum(train_accuracy) / len(train_accuracy)))
+                    print('Average train multi label accuracy: {:.3f}'.format(sum(train_multi_accuracy) / len(train_multi_accuracy)))
 
                 _, y_pred = logits.detach().topk(num_of_labels)
                 y_pred = y_pred.view(-1)
@@ -217,14 +172,14 @@ def main():
                 img_orig = Image.fromarray(img)
                 img_orig.save(dir_path + '/' + 'orig.jpg')
 
-                htm = heatmap.squeeze().cpu().detach().numpy()
+                # htm = heatmap.squeeze().cpu().detach().numpy()
                 #plt.imshow(htm)
                 #plt.show()
 
-                htm = deprocess_image(htm)
-                visualization, heatmap = show_cam_on_image(img, htm, True)
-                visualization_m = Image.fromarray(visualization)
-                visualization_m.save(dir_path+'/'+'vis.jpg')
+                # htm = deprocess_image(htm)
+                # visualization, heatmap = show_cam_on_image(img, htm, True)
+                # visualization_m = Image.fromarray(visualization)
+                # visualization_m.save(dir_path+'/'+'vis.jpg')
                 #plt.imshow(visualization)
                 #plt.show()
                 #plt.imshow(heatmap)
@@ -243,6 +198,9 @@ def main():
                     plt.plot(mean_train_accuracy)
                     plt.savefig(epoch_path + '/train_accuracy.jpg')
                     plt.close()
+                    plt.plot(mean_train_multi_accuracy)
+                    plt.savefig(epoch_path + '/train_multi_accuracy.jpg')
+                    plt.close()
             i+=1
 
 
@@ -253,7 +211,7 @@ def main():
         plt.plot(total_train_loss)
         plt.savefig(viz_path + '/total_train_accuracy.jpg')
 
-        test_path = 'C:/Users/Student1/PycharmProjects/GCAM/viz/test'
+        test_path = 'C:/Users/Student2/PycharmProjects/GCAM/exp1/test'
         pathlib.Path(test_path).mkdir(parents=True, exist_ok=True)
         epoch_path = test_path + '/epoch_' + str(epoch)
         pathlib.Path(epoch_path).mkdir(parents=True, exist_ok=True)
@@ -262,73 +220,44 @@ def main():
         i = 0
         for sample in rds.datasets['test']:
             input_tensor = preprocess_image(sample['image'].squeeze().numpy(), mean=mean, std=std).to(device)
-            # input_tensor2 = torch.from_numpy(sample['image'].copy()).unsqueeze(0).permute([0, 3, 1, 2]).to(device).float()
-            # np_im2 = input_tensor.squeeze().permute(1, 2, 0).cpu()
             label_idx_list = sample['label/idx']
             num_of_labels = len(label_idx_list)
-
-            """
-            #this code used for initial experiments  GCAM visualization
-            print(i)
-            if i % 100 == 0 :
-                img = sample['image']
-                plt.imshow(img)
-                plt.show()
-
-                for label_idx in label_idx_list:
-                    print(categories[label_idx])
-
-
-                    gcam.forward(input_tensor)
-                    ids_ = torch.LongTensor([[label_idx]] * len(input_tensor)).to(device)
-                    gcam.backward(ids=ids_)
-                    regions = gcam.generate(target_layer='features')
-                    regions_cpu = regions.squeeze().cpu()
-                    visualization, heatmap = show_cam_on_image(img, regions_cpu, True)
-                    plt.imshow(visualization)
-                    plt.show()
-                    plt.imshow(heatmap)
-                    plt.show()
-
-
-                    '''
-                    cam = GradCAM(model=model, target_layer=target_layer, use_cuda=True)
-                    grayscale_cam = cam(input_tensor=input_tensor, target_category=label_idx)
-                    heatmap = cv2.applyColorMap(np.uint8(255 * grayscale_cam), cv2.COLORMAP_JET)
-                    heatmap = np.float32(heatmap) / 255
-                    plt.imshow(heatmap)
-                    plt.show()
-                    visualization = show_cam_on_image(img, heatmap)
-                    plt.imshow(visualization)
-                    plt.show()
-                    print()
-                    '''
-            """
-
             labels = torch.Tensor(label_idx_list).to(device).long()
-            logits, heatmap = gcam(input_tensor, labels)
-
+            logits = model(input_tensor)
             indices = torch.Tensor(label_idx_list).long().to(device)
             class_onehot = torch.nn.functional.one_hot(indices, num_classes).sum(dim=0).unsqueeze(0).float()
-
             loss = loss_fn(logits, class_onehot)
+
+            # Single label evaluation
+            y_pred = logits.detach().argmax()
+            y_pred = y_pred.view(-1)
+            gt, _ = indices.sort(descending=True)
+            gt = gt.view(-1)
+            acc = (y_pred == gt).sum()
+            total_test_single_accuracy += acc
+
+            # Multi label evaluation
+            _, y_pred_multi = logits.detach().topk(num_of_labels)
+            y_pred_multi = y_pred_multi.view(-1)
+            acc_multi = (y_pred_multi == gt).sum() / num_of_labels
+            total_test_multi_accuracy += acc_multi
 
             if i % 100 == 0:
                 print(i)
                 print('Loss per image: {:.3f}'.format(loss.detach().item()))
 
-                _, y_pred = logits.detach().topk(num_of_labels)
-                y_pred = y_pred.view(-1)
-                gt, _ = indices.sort(descending=True)
-                gt = gt.view(-1)
-                acc = (y_pred == gt).sum() / num_of_labels
                 test_accuracy.append(acc)
+                test_multi_accuracy.append(acc_multi)
+
 
             if i % 200 == 0:
                 test_epoch_loss.append(loss.detach().item())
                 if len(train_accuracy) > 1:
                     mean_test_accuracy.append(sum(test_accuracy) / len(test_accuracy))
-                    print('Average accuracy: {:.3f}'.format(sum(test_accuracy) / len(test_accuracy)))
+                    mean_test_multi_accuracy.append(sum(test_multi_accuracy) / len(test_multi_accuracy))
+                    print('Average test single label accuracy: {:.3f}'.format(sum(test_accuracy) / len(test_accuracy)))
+                    print('Average test multi label accuracy: {:.3f}'.format(sum(test_multi_accuracy) / len(test_multi_accuracy)))
+
 
                 _, y_pred = logits.detach().topk(num_of_labels)
                 y_pred = y_pred.view(-1)
@@ -346,14 +275,14 @@ def main():
                 img_orig = Image.fromarray(img)
                 img_orig.save(dir_path + '/' + 'orig.jpg')
 
-                htm = heatmap.squeeze().cpu().detach().numpy()
+                # htm = heatmap.squeeze().cpu().detach().numpy()
                 # plt.imshow(htm)
                 # plt.show()
 
-                htm = deprocess_image(htm)
-                visualization, heatmap = show_cam_on_image(img, htm, True)
-                visualization_m = Image.fromarray(visualization)
-                visualization_m.save(dir_path + '/' + 'vis.jpg')
+                # htm = deprocess_image(htm)
+                # visualization, heatmap = show_cam_on_image(img, htm, True)
+                # visualization_m = Image.fromarray(visualization)
+                # visualization_m.save(dir_path + '/' + 'vis.jpg')
                 # plt.imshow(visualization)
                 # plt.show()
                 # plt.imshow(heatmap)
@@ -371,7 +300,39 @@ def main():
                     plt.plot(mean_test_accuracy)
                     plt.savefig(epoch_path + '/test_accuracy.jpg')
                     plt.close()
+                    plt.plot(mean_test_multi_accuracy)
+                    plt.savefig(epoch_path + '/test_multi_accuracy.jpg')
+                    plt.close()
+
             i+=1
+
+        print("finished epoch number:")
+        print(epoch)
+
+        epoch_train_single_accuracy.append(total_train_single_accuracy / num_train_samples)
+        print('Average epoch single train accuracy: {:.3f}'.format(total_train_single_accuracy / num_train_samples))
+
+        epoch_train_multi_accuracy.append(total_train_multi_accuracy / num_train_samples)
+        print('Average epoch multi train accuracy: {:.3f}'.format(total_train_multi_accuracy / num_train_samples))
+
+        epoch_test_single_accuracy.append(total_test_single_accuracy / num_test_samples)
+        print('Average epoch single test accuracy: {:.3f}'.format(total_test_single_accuracy / num_test_samples))
+
+        epoch_test_multi_accuracy.append(total_test_multi_accuracy / num_test_samples)
+        print('Average epoch multi test accuracy: {:.3f}'.format(total_test_multi_accuracy / num_test_samples))
+
+        plt.plot(epoch_train_single_accuracy)
+        plt.savefig(viz_path + '/epoch_train_single_accuracy.jpg')
+        plt.close()
+        plt.plot(epoch_train_multi_accuracy)
+        plt.savefig(viz_path + '/epoch_train_multi_accuracy.jpg')
+        plt.close()
+        plt.plot(epoch_test_single_accuracy)
+        plt.savefig(viz_path + '/epoch_test_single_accuracy.jpg')
+        plt.close()
+        plt.plot(epoch_test_multi_accuracy)
+        plt.savefig(viz_path + '/epoch_test_multi_accuracy.jpg')
+        plt.close()
 
         total_test_loss.append(sum(test_epoch_loss) / len(test_epoch_loss))
         plt.plot(total_test_loss)

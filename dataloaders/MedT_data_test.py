f21317ffbe03b2bd37b9135472474268cb8cf0e4
ilyak93 <ilyak93@gmail.com>
2021-05-06 21:18:35 +0000

    some little and simple additions

--- dataloaders/MedT_data_test.py
+++ dataloaders/MedT_data_test.py
@@ -2,7 +2,7 @@ from dataloaders.MedTData import MedT_Loader
 
 medt_loader = MedT_Loader('C:/MDT_dataset/SB3_ulcers_mined_roi_mult/')
 
-'''
+''' infinite for
 for sample in medt_loader.datasets['train']:
     s = sample
     if s[1].size() == s[0].size()[0:3]:
@@ -10,12 +10,15 @@ for sample in medt_loader.datasets['train']:
     else :
         print('without_mask')
 '''
-
 for sample in medt_loader.datasets['test']:
     s = sample
     if s[1].size() == s[0].size()[0:3]:
         print('with_mask')
     else :
         print('without_mask')
+        if s[2] == 1:
+            print('Pos')
+        else:
+            print('Neg')
 
 print()

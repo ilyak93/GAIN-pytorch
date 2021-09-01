ab50c39b6630d0e756464d387420588c93fd1d13
ilyak93 <ilyak93@gmail.com>
2021-09-01 20:10:43 +0300

    Update main_VOC_multibatch_multilabel_heatmaps_singlebatch.py

--- main_VOC_multibatch_multilabel_heatmaps_singlebatch.py
+++ main_VOC_multibatch_multilabel_heatmaps_singlebatch.py
@@ -84,7 +84,7 @@ def main():
     # chkpnt_epoch = checkpoint['epoch']+1
 
     writer = SummaryWriter(
-        "/content/drive/MyDrive/VOC-dataset/VOC-logs" + "/VOC_multibatch_multiheatmaps_GAIN_singlebatch" +
+        "/content/logs" + "/VOC_multibatch_multiheatmaps_GAIN_singlebatch" +
             datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
     i=0
     num_train_samples = 0

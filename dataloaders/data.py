5de351bad4658f9f3d750c35aabac0e703d66a89
ilyak93 <ilyak93@gmail.com>
2021-09-01 19:22:34 +0300

    gpu to cpu and vice versa
    device_name parameter for changing from gpu to cpu and vice versa

--- dataloaders/data.py
+++ dataloaders/data.py
@@ -5,7 +5,8 @@ from torch.utils.data import Dataset, Subset
 from dataloaders.pascal_voc_loader import PascalVOCLoader
 from torch.utils.data.sampler import SubsetRandomSampler, SequentialSampler
 
-device = torch.device('cuda:0')
+device_name = 'cpu' # cuda:0
+device = torch.device(device_name)
 
 def my_collate(batch):
     images = [torch.tensor(sample['image']) for sample in batch]

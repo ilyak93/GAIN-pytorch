4f9f6ee138ea75dd6cdb02243d916d9e0a5b7249
ilyak93 <ilyak93@gmail.com>
2021-09-01 20:09:12 +0300

    Update main_VOC_multibatch_multilabel_heatmaps_singlebatch.py

--- main_VOC_multibatch_multilabel_heatmaps_singlebatch.py
+++ main_VOC_multibatch_multilabel_heatmaps_singlebatch.py
@@ -101,8 +101,6 @@ def main():
         epoch_train_cl_loss = 0
 
         model.train(True)
-        
-        print('started')
 
         if not test_first_before_train or (test_first_before_train and epoch != 0):
 
@@ -129,9 +127,6 @@ def main():
 
                 labels = sample[2]
                 
-                print('first forward')
-
-
                 logits_cl, logits_am, heatmap, masked_image, mask = gain(batch, sample[1])
 
                 class_onehot = torch.stack(sample[1]).float()

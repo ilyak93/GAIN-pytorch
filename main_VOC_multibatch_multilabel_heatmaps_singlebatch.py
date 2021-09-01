7b27104a332e84dbe9e686147ad740e1b1c6a843
ilyak93 <ilyak93@gmail.com>
2021-09-01 20:51:40 +0300

    Update main_VOC_multibatch_multilabel_heatmaps_singlebatch.py

--- main_VOC_multibatch_multilabel_heatmaps_singlebatch.py
+++ main_VOC_multibatch_multilabel_heatmaps_singlebatch.py
@@ -73,7 +73,7 @@ def main():
 
     epochs = 100
     loss_fn = torch.nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
     gain = batch_GAIN_VOC_multiheatmaps(model=model, grad_layer='features', num_classes=num_classes, pretraining_epochs=1,
                 test_first_before_train=test_first_before_train)
 
